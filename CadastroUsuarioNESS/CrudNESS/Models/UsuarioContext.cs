abfac987733641bb935320029d8ad0384546b5e8
Frank Vitor <frank.vitor2013@gmail.com>
2020-03-06 00:07:32 -0300

    add connectionstring dependencia

---
+++ CadastroUsuarioNESS/CrudNESS/Models/UsuarioContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNESS.Models
+{
+    public class UsuarioContext : DbContext 
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options ) : base(options)
+        {
+        }
+    }
+}
